bc5d03344b0910461f734b04ebb1b03e2355997c
Luke Parker <lukeparker1988@Lukes-MacBook-Pro.local>
2015-01-20 13:37:50 +0000

    Added the Player Class and started on the player info.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private string name;
+	private float invisibilityDuration;
+	private float invisibilityCharge;
+	private float currentChargeOrDuration;
+	private bool hasCharged;
+
+	public bool isCharged { get; private set; }
+	public bool isInvisible { get; private set; }
+
+	public Player(string playerName,float invisDuration, float invisCharge)
+	{
+		name = playerName;
+		invisibilityDuration = invisDuration;
+		invisibilityCharge = invisCharge;
+		currentChargeOrDuration = invisCharge;
+		hasCharged = true;
+	}
+}
