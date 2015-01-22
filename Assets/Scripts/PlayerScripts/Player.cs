005865d2f2b48435c9b64767fd05c3e27e7a7032
Luke Parker <lparker@moongate-uk.co.uk>
2015-01-22 14:10:57 +0000

    Completed the Player Class, Added a PlayerControl Class.

---
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	//Directly Effect the Player.
+	public string PlayerName = "";
+	public float InvisibilityCharge = 5.0f;
+	public float CurrentChargeOrDuration = 5.0f;
+	public bool IsCharged = true;
+	public bool IsInvisible = false;
+	private float chargeSpeed = 0.1f;
+	private float useSpeed = 0.2f;
+
+
+	//Stats
+	private float distanceTravelled;
+	private int dungeonsCompleted;
+	private int lootCollected;
+	private int timesSeen;
+
+	public void TriggerInvisibility()
+	{
+		if (!IsInvisible && IsCharged) 
+		{
+			IsInvisible = true;
+		}
+	}
+
+	public void ChargingOrUsingInvisibility()
+	{
+		if (IsInvisible) 
+		{
+			if(CurrentChargeOrDuration <= 0)
+			{
+				IsInvisible = true;
+				IsCharged = false;
+			}
+			CurrentChargeOrDuration -= useSpeed;
+		}
+		else if (!IsInvisible && !IsCharged)
+		{
+			if(CurrentChargeOrDuration < InvisibilityCharge)
+			{
+				CurrentChargeOrDuration += chargeSpeed;
+			}
+			else if (CurrentChargeOrDuration >= InvisibilityCharge)
+			{
+				IsCharged = true;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	private float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis ("Vertical") > 0)
+		{
+			MovePlayer ();
+		}
+	}
+
+	void FixedUpdate(){
+
+	}
+
+	void MovePlayer(){
+		rigidbody.AddForce (Vector3.forward * speed * Time.deltaTime,ForceMode.Acceleration);
+	}
+
+	void RotatePlayer(){
+
+	}
+}
